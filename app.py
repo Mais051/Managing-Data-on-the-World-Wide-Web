98390439cf79842409fafd225921176221d8cf35
liralezra <liralezra@campus.technion.ac.il>
2019-06-14 15:54:03 +0300

    basic example of using postgresql

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345678@localhost/postgres'
+app.debug = True
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(12))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("add_user.html")
+
+
+@app.route('/post_user', methods=['POST'])
+def post_user():
+    user = User(request.form['username'], request.form['password'])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':

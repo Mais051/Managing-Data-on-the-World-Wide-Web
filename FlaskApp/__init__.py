24ac85797f9d83a109251545828ecd545b7e8094
liralezra <liralezra@campus.technion.ac.il>
2019-08-05 13:47:43 +0300

    basic example of using postgresql

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345678@localhost/postgres'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.debug = True
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-from FlaskProject import routes
+from FlaskApp import routes

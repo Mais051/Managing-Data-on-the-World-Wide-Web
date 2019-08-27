48c25dfc1e3df0b391c706cde022302d32816b3d
liralezra <liralezra@campus.technion.ac.il>
2019-08-27 14:50:46 +0300

    delete post added

--- FlaskApp/routes.py
+++ FlaskApp/routes.py
@@ -169,7 +169,7 @@ def delete_post_user(travel_id):
     db.session.delete(post)
     db.session.commit()
     flash('Your post has been deleted!', 'success')
-    return redirect(url_for('user_account', post.traveler.id))
+    return redirect(url_for('user_account', user_id=post.traveler.id))
 
 
 @app.route("/post/<int:travel_id>/delete", methods=['POST'])
